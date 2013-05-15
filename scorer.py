386a1fdd053fb8fca2537b2f3d8a2678e06d24e1
Wil Collins <wil.collins@vt.edu>
2013-05-15 02:12:59 -0300

    Update scorer.py

--- scorer.py
+++ scorer.py
@@ -1,54 +1,53 @@
 import nltk
-import codecs
-from fcutils import tokenizeDocText
+from nltk.stem.porter import PorterStemmer
+from gensim import corpora, models, similarities
+from nltk.tokenize.regexp import WordPunctTokenizer
+#from nltk.corpus import wordnet
+import re
+from collections import Counter
+from itertools import repeat
 
 class Scorer(object):
-    """ Base class for VSM scorers """
- 
-    def __init__(self, docTexts):
+    def __init__(self):
+        self.stemmer = PorterStemmer()
+        self.stopwords = nltk.corpus.stopwords.words('english')
+        self.tokenizer = WordPunctTokenizer()
         self.keywords = []
-        self.dictionary = None;
-        self.model = None;
-        self.similarityModel = None
-        self.constructModel(docTexts)
+        self.score = 0
 
-    def constructModel(self, docTexts):
-        """ Given document texts, constructs the VSM and similarity models"""
-        return
-
+    def __init__(self,keywords):
+        self.stemmer = PorterStemmer()
+        self.stopwords = nltk.corpus.stopwords.words('english')
+        self.tokenizer = WordPunctTokenizer()
+        self.score = 0
+        keywords = ['boston','marathon','bombing', 'dzhokhar', 'tamerlan', 'tsarnaev','april', 'massachusetts', 'mit']
+        self.keywords = keywords
+        print(keywords)
+                
+    def cleanDoc(self,doc, count = 3):
+        tokens = self.tokenizer.tokenize(doc)
+        clean = [token.lower() for token in tokens if token.lower() not in self.stopwords and len(token) > 2 and tokens.count(token) > count]
+        domains = ["guardian", "guardiannew", "cnn", "fbi", "npr", "wbur", "nytimes", "com","twitter","retweet", "via"]
+        stopwords = ["said", "say", "told", "comment", "saw", "www", "http", "href", "blockquote", "other", "post"]
+        clean = [dat for dat in clean if dat not in domains and dat not in stopwords and re.match("^[A-Za-z0-9]*$", dat)] #and wordnet.synsets(dat)] # and not (re.match("^[A-Za-z]+$", dat) and re.match("^[0-9]+$",dat))]
+        final = [self.stemmer.stem(word) for word in clean]
+        return final
     
-    def calculate_score(self,docText):
-        """ Given document text, returns relevancy score.
-        Document text is tokenized, transformed into vector space,
-        and then the maximum dot-product is returned"""
-
-        docTokens = tokenizeDocText(docText)
+    def normalize_list(self,L, threshold):
+        cntr = Counter(L)
+        least_count = cntr.most_common()[-1][1]
+        if least_count > threshold:
+            least_count -= 1
+        cntr.subtract([item for k in cntr.keys() for item in list(repeat(k, least_count))])
+        return list(cntr.elements())
         
-        # transform document into model's vector space
-        doc_bow = self.dictionary.doc2bow(docTokens)
-        vec = self.model[doc_bow]
-
-        # return maximum similarity (dot products)
-        simList = self.similarityModel[vec]
-        return max(simList)
-
-
-    def labelDocs(self, docNames, minSize, irrelThresh, relThresh):
-        """ Labels a list of documents as relevant (score >= relThresh) or non-relevant (score <= irrelThresh). """
-        
-        relevantDocNames = []
-        irrelevantDocNames = []
-        for docName in docNames:
-            f = codecs.open(docName, "r")
-            text = f.read()
-            f.close()
-            score = self.calculateScore(text)
-            if score <= irrelThresh:
-                irrelevantDocNames.append(docName)
-            elif score >= relThresh:
-                relevantDocNames.append(docName)
-        if len(relevantDocNames) < minSize:
-            raise Exception("Not enough relevant documents")
-        if len(irrelevantDocNames) < minSize:
-            raise Exception("Not enough irrelevant documents")
-        return relevantDocNames, irrelevantDocNames
+# this function checks if the url words contain the keywords or not.
+# the score given is calculated by finding how many keywords occur in the url.
+    def calculate_score(self,url):
+        words = url.getAllText().split('-')
+        print(words)        
+        for w in self.keywords:
+            if w in words:
+                self.score +=1
+        self.score = self.score / float(len(self.keywords))
+        return self.score
