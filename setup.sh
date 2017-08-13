299505899152bdc185c3755b9fd8c0366c3738d7
Wil Collins <wil.collins@gmail.com>
2017-08-12 23:27:33 -0700

    Update setup script with PIP & NPM install

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+pip install -r "$DIR/requirements.txt"
 sudo python -m nltk.downloader -d /usr/local/share/nltk_data all
+cd server && npm install
